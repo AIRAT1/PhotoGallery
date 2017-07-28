ebc4b8a5a03219e47e0a34755102f1e5ed42bc0a
AIRAT1 <ayrat1@mail.ru>
2017-07-28 16:44:16 +0300

    complete 481

--- app/src/main/java/de/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/de/android/photogallery/FlickrFetchr.java
@@ -18,6 +18,15 @@ import java.util.List;
 public class FlickrFetchr {
     private static final String TAG = FlickrFetchr.class.getSimpleName();
     private static final String API_KEY = "9771742d110b76b54fd477ae53beed1a";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri.parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -29,8 +38,8 @@ public class FlickrFetchr {
 
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 throw new IOException(connection.getResponseMessage() +
-                ": with "
-                + urlSpec);
+                        ": with "
+                        + urlSpec);
             }
 
             int bytesRead = 0;
@@ -40,37 +49,48 @@ public class FlickrFetchr {
             }
             out.close();
             return out.toByteArray();
-        }finally {
+        } finally {
             connection.disconnect();
         }
     }
 
-    public String getUrlString(String urlSpec) throws IOException{
+    public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public List<GalleryItem> fetchItems() {
+    public List<GalleryItem> fetchRecentPhotos() {
+        String url = buildUrl(FETCH_RECENTS_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query) {
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> downloadGalleryItems(String url) {
         List<GalleryItem> items = new ArrayList<>();
 
         try {
-            String url = Uri.parse("https://api.flickr.com/services/rest/")
-                    .buildUpon()
-                    .appendQueryParameter("method", "flickr.photos.getRecent")
-                    .appendQueryParameter("api_key", API_KEY)
-                    .appendQueryParameter("format", "json")
-                    .appendQueryParameter("nojsoncallback", "1")
-                    .appendQueryParameter("extras", "url_s")
-                    .build().toString();
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON: " + jsonString);
             JSONObject jsonBody = new JSONObject(jsonString);
             parseItems(items, jsonBody);
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             Log.e(TAG, "Failed to parse JSON", e);
-        }catch (IOException e) {
+        } catch (IOException e) {
             Log.e(TAG, "Failed to fetch items", e);
         }
         return items;
+    }
+
+    private String buildUrl(String method, String query) {
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+        return uriBuilder.toString();
     }
 
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws JSONException {
@@ -82,7 +102,8 @@ public class FlickrFetchr {
 
             GalleryItem item = new GalleryItem();
             item.setId(photoJsonObject.getString("id"));
-            item.setCaption(photoJsonObject.getString("title"));;
+            item.setCaption(photoJsonObject.getString("title"));
+            ;
 
             if (!photoJsonObject.has("url_s")) {
                 continue;
