469f81bf5714f2ec89d7541a948c16c19604caca
AIRAT1 <ayrat1@mail.ru>
2017-08-01 18:02:13 +0300

    complete 525

--- app/src/main/java/de/android/photogallery/StartupReceiver.java
+++ app/src/main/java/de/android/photogallery/StartupReceiver.java
@@ -11,7 +11,6 @@ public class StartupReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Received broadcast intent: " + intent.getAction());
-
         boolean isOn = QueryPreferences.isAlarmOn(context);
         PollService.setServiceAlarm(context, isOn);
     }
