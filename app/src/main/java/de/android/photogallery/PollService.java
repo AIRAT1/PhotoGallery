8cbaa3ad505a2e9fdac13ee9c7734a5eacc3d65e
AIRAT1 <ayrat1@mail.ru>
2017-08-01 18:00:37 +0300

    complete 525

--- app/src/main/java/de/android/photogallery/PollService.java
+++ app/src/main/java/de/android/photogallery/PollService.java
@@ -39,6 +39,8 @@ public class PollService extends IntentService{
         }else {
             alarmManager.cancel(pi);
         }
+
+        QueryPreferences.setAlarmOn(context, isOn);
     }
 
     @Override
