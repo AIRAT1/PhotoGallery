39d36e5f3a939e80abf49d0ccca770f7743c28ef
AIRAT1 <ayrat1@mail.ru>
2017-08-01 17:52:16 +0300

    complete 519

--- app/src/main/java/de/android/photogallery/PollService.java
+++ app/src/main/java/de/android/photogallery/PollService.java
@@ -2,19 +2,23 @@ package de.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
 
 public class PollService extends IntentService{
     private static final String TAG = PollService.class.getSimpleName();
-    private static final int POLL_INTERVAL = 1000 * 60;
+//    private static final int POLL_INTERVAL = 1000 * 60;
+    private static final long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
     public PollService() {
         super(TAG);
@@ -64,6 +68,21 @@ public class PollService extends IntentService{
             Log.i(TAG, "Got a new result:" + resultId);
         }
 
+        Resources resources = getResources();
+        Intent i = PhotoGalleryActivity.newIntent(this);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+        Notification notification = new Notification.Builder(this)
+                .setTicker(resources.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(resources.getString(R.string.new_pictures_title))
+                .setContentText(resources.getString(R.string.new_pictures_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, notification);
+
         QueryPreferences.setLastResultId(this, resultId)    ;
     }
 
@@ -72,5 +91,11 @@ public class PollService extends IntentService{
         boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
         boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
         return isNetworkConnected;
+    }
+
+    public static boolean isServiceAlarmOn(Context context) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
     }
 }
