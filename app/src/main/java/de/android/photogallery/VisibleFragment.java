b62f37289c516103eb1cc66b3e6651e18559a139
Ayrat <ayrat1@mail.ru>
2017-08-01 22:42:07 +0300

    complete 541

--- app/src/main/java/de/android/photogallery/VisibleFragment.java
+++ app/src/main/java/de/android/photogallery/VisibleFragment.java
@@ -1,12 +1,13 @@
 package de.android.photogallery;
 
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
-import android.widget.Toast;
+import android.util.Log;
 
 public abstract class VisibleFragment extends Fragment{
     private static final String TAG = VisibleFragment.class.getSimpleName();
@@ -15,7 +16,7 @@ public abstract class VisibleFragment extends Fragment{
     public void onStart() {
         super.onStart();
         IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
-        getActivity().registerReceiver(onShowNotification, filter);
+        getActivity().registerReceiver(onShowNotification, filter, PollService.PERM_PRIVATE, null);
     }
 
     @Override
@@ -27,7 +28,9 @@ public abstract class VisibleFragment extends Fragment{
     private BroadcastReceiver onShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(), "Got a broadcast: " + intent.getAction(), Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getActivity(), "Got a broadcast: " + intent.getAction(), Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "cancelling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }
