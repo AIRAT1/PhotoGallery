ff45d6125da7dccd2f6cadee2ac054a146335852
Ayrat <ayrat1@mail.ru>
2017-08-01 22:47:42 +0300

    complete 543

--- app/src/main/java/de/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/de/android/photogallery/FlickrFetchr.java
@@ -103,12 +103,12 @@ public class FlickrFetchr {
             GalleryItem item = new GalleryItem();
             item.setId(photoJsonObject.getString("id"));
             item.setCaption(photoJsonObject.getString("title"));
-            ;
 
             if (!photoJsonObject.has("url_s")) {
                 continue;
             }
             item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
             items.add(item);
         }
     }
